000560655915cbacbb3061df4a2eaaa4c824ab22
aGiBa 🚀 <mohamed.Agiba@cic.ae>
2026-03-01 04:03:37 +0200

    feat(app): restructure app layout and add server dependencies
    - Replace single to-do component with header, footer, and router outlet
    - Add multiple new Angular components (home, todo, about-us, contactus, etc.)
    - Install json-server and related dependencies for backend API
    - Set up initial database structure with sample todos
    - Update component structure for multi-page application
    
    V.3.WIP

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,17 @@
 import { Routes } from '@angular/router';
+import { Home } from './Components/home/home';
+import { Todo } from './Components/todo/todo';
+import { TodoDetails } from './Components/todo-details/todo-details';
+import { Contactus } from './Components/contactus/contactus';
+import { AboutUs } from './Components/about-us/about-us';
+import { NotFound } from './Components/not-found/not-found';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path:"", redirectTo:'/todo', pathMatch:'full'},
+    {path:'home', component:Home, title:'Home'},
+    {path:'aboutus', component:AboutUs, title:'AboutUs'},
+    {path:'todo', component:Todo, title:'Todo'},
+    {path:'todo/:id', component:TodoDetails, title:'Todo Details'},
+    {path:'contactUs', component:Contactus, title:'ContactUs'},
+    {path:'**', component:NotFound, title:'Not Found'},
+];
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,25 +1,16 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { ToDo } from './Components/to-do/to-do';
+import { Header } from './Components/header/header';
+import { Footer } from './Components/footer/footer';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, ToDo],
+  imports: [RouterOutlet,
+      Header, Footer
+  ],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
 export class App {
-protected tasks = signal<string[]>([]);
 
-addTask(task:string){
-  if(task.trim()){
-    this.tasks.update(tasks => [...tasks, task]);
-    console.log('Current List',this.tasks())
-  }
-}
-deleteTask(index: number) {
-  this.tasks.update(oldTasks => 
-    oldTasks.filter((_, i) => i !== index) 
-  );
-}
 }
